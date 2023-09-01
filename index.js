7bb7e94d1c9db8405c6a6a02ea557549c573475d
shashanksb17 <sbasutkar999@gmail.com>
2023-09-01 18:13:45 +0530

    project schema & model created

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-const express= require("express")
-require("dotenv").config()
-const cors=require("cors")
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
 
-const {db_connection}=require("./config/db")
+const { db_connection } = require("./config/db");
 
-const app=express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use("/",(req,res)=>{
-    res.send("HOME PAGE OF TRIVEOUS-ECOMMERCE APP")
-})
+app.use("/", (req, res) => {
+  res.send("HOME PAGE OF TRIVEOUS-ECOMMERCE APP");
+});
 
-app.listen(process.env.PORT,async()=>{
-    try{
-        await db_connection
-        console.log("connected to DB")
-    }
-    catch(err){
-        console.log("can not connect DB")
-        console.log(err)
-    }
-    console.log(`server is running at port ${process.env.port}`)
-})
+app.listen(process.env.PORT, async () => {
+  try {
+    await db_connection;
+    console.log("connected to DB");
+  } catch (err) {
+    console.log("can not connect DB");
+    console.log(err);
+  }
+  console.log(`server is running at port ${process.env.port}`);
+});
